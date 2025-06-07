206e0485c61c90411b86e4e4dfab317540e6f938
Reggie-sun <sunruijie42@gmail.com>
2025-06-07 22:31:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,23 @@ with st.expander('Data visualization'):
 ).interactive()
   
   st.altair_chart(chart, use_container_width=True)
+
+def slider():
+
+  slider_lables=[("Island", "island"),
+("Bill Length (mm)", "bill_length_mm"),
+("Bill Depth (mm)", "bill_depth_mm"),
+("Flipper Length (mm)", "flipper_length_mm"),
+("Body Mass (g)", "body_mass_g"),
+("Sex", "sex"),]
+
+inout_data={}
+
+for lables,key in slider_lables:
+  st.slider(laber,min_value=float(0), max_value=float(df[key].max()), value=float(df[key].mean())
+  
+  
+
+
+
+
